db251da8c7dd5982766317034ef5266d3a8d7ca2
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 22:55:25 +0300

    Delcared and defined the GetBallContours functions in the main python file

--- CVBallTrackingGame.py
+++ CVBallTrackingGame.py
@@ -10,6 +10,41 @@
 import HSVColorDetector
 import cv2
 
+
+# A function that's going to return the ball contours
+def GetBallContours(frame, findFrame, minArea=1000, sort=True, filter=0, DrawContour=True, c=(255, 0, 0)):
+
+    ContoursFound = []
+    ImageContours = frame.copy()
+    contours, _ = cv2.findContours(
+        findFrame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > minArea:
+            peri = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+
+            if len(approx) == filter or filter == 0:
+                if DrawContour:
+                    cv2.DrawContourtours(ImageContours, contour, -1, c, 3)
+                x, y, w, h = cv2.boundingRect(approx)
+                cx, cy = x + (w // 2), y + (h // 2)
+                cv2.rectangle(ImageContours, (x, y), (x + w, y + h), c, 2)
+                cv2.circle(ImageContours, (x + (w // 2),
+                           y + (h // 2)), 5, c, cv2.FILLED)
+                ContoursFound.append({"contour": contour, "area": area, "bbox": [
+                                     x, y, w, h], "center": [cx, cy]})
+
+    # Executes if the sorted flag is true
+    if sort:
+        ContoursFound = sorted(
+            ContoursFound, key=lambda x: x["area"], reverse=True)
+
+    # Returning the lists created above
+    return ImageContours, ContoursFound
+
+
 # Initializing the webcam input
 CamVideo = cv2.VideoCapture(0)
 # Setting the OpenCV Window Size
