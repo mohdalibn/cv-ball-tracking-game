b55fb327c7ba4bd6883dea14afba6e149f4748ff
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 20:43:09 +0300

    Added GetColorHSVVals & UpdateFrame methods will required code

--- HSVColorDetector.py
+++ HSVColorDetector.py
@@ -27,7 +27,7 @@ class ColorDetector:
         cv2.createTrackbar("Val Min", "TrackBars", 0, 255, self.empty)
         cv2.createTrackbar("Val Max", "TrackBars", 255, 255, self.empty)
 
-    # This method will
+    # This method will get all the control values from the trackbars
     def GetColorControlVals(self):
         HueMin = cv2.getTrackbarPos("Hue Min", "TrackBars")
         SatMin = cv2.getTrackbarPos("Sat Min", "TrackBars")
@@ -44,3 +44,52 @@ class ColorDetector:
 
         # This will return the the values
         return HSVValues
+
+    # This method will get HSV Values of the color that's passed as an argument to this function
+    def GetColorHSVVals(self, Color):
+
+        if Color == 'red':
+            OutputHSV = {'HueMin': 146, 'SatMin': 141, 'ValMin': 77,
+                         'HueMax': 179, 'SatMax': 255, 'ValMax': 255}
+        elif Color == 'green':
+            OutputHSV = {'HueMin': 44, 'SatMin': 79, 'ValMin': 111,
+                         'HueMax': 79, 'SatMax': 255, 'ValMax': 255}
+        elif Color == 'blue':
+            OutputHSV = {'HueMin': 103, 'SatMin': 68, 'ValMin': 130,
+                         'HueMax': 128, 'SatMax': 255, 'ValMax': 255}
+        else:
+            # Setting the OutputHSV to None if the argument color is not present above
+            OutputHSV = None
+            logging.warning("Color Not Defined")
+            logging.warning("Available colors: red, green, blue ")
+
+        # Returning the resulting HSV values
+        return OutputHSV
+
+    # This method updates the Image frame according to the HSV Color Value that's passed as a parameter to this function
+    def UpdateFrame(self, frame, Color=None):
+
+        # Variable to store the Masked Image and the Color Detected Image
+        MaskedImage = []  # The Masked Image will highlight the detected color in white
+        ColoredImage = []  # The Colored Image will only show the major detected color
+
+        # If the Color Control Trackbars flag is set to true, this statement executes
+        if self.ColorControls:
+            Color = self.GetColorControlVals()
+
+        # This statement executes if only the name of the color is specified as the Color parameter to this function
+        if isinstance(Color, str):
+            Color = self.GetColorHSVVals(Color)
+
+        # If the passed Color argument is not none nor a string, this block of code is executed
+        if Color is not None:
+            ImageHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            LowerHSVVals = np.array(
+                [Color['HueMin'], Color['SatMin'], Color['ValMin']])
+            UpperHSVVals = np.array(
+                [Color['HueMax'], Color['SatMax'], Color['ValMax']])
+            MaskedImage = cv2.inRange(ImageHSV, LowerHSVVals, UpperHSVVals)
+            ColoredImage = cv2.bitwise_and(frame, frame, mask=MaskedImage)
+
+        # Return the Masked & Colored Images
+        return MaskedImage, ColoredImage
