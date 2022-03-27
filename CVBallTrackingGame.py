f58c926775ddb1702e4020728b9756d87fa09072
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 21:02:38 +0300

    Initialized the webcam input and set the window size

--- CVBallTrackingGame.py
+++ CVBallTrackingGame.py
@@ -9,3 +9,9 @@
 # Importing the required libraries for the project
 from HSVColorDetector import ColorDetector
 import cv2
+
+# Initializing the webcam input
+CamVideo = cv2.VideoCapture(0)
+# Setting the OpenCV Window Size
+CamVideo.set(3, 640)
+CamVideo.set(4, 480)
