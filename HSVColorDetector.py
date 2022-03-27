45711a87905eff90885e34a8e428118831975644
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 19:13:37 +0300

    Add the __init__ function

--- HSVColorDetector.py
+++ HSVColorDetector.py
@@ -7,4 +7,10 @@ import cv2
 
 # Class that contains all the relevant methods to use this file in other projects
 class ColorDetector:
-    pass
+    def __init__(self, ColorControls=False):
+        self.ColorControls = ColorControls
+
+        if self.ColorControls:
+            self.OpenControls()
+
+    # This function will the color controls window
