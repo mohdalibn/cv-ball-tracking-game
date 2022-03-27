367d7ecbdb02f7f0a9cf6a49a63da9c17a5ce676
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 19:28:51 +0300

    Declared and defined the GetColorControlVals method

--- HSVColorDetector.py
+++ HSVColorDetector.py
@@ -13,14 +13,34 @@ class ColorDetector:
         if self.ColorControls:
             self.OpenControls()
 
-    # This function will the color controls window
+    # This method will the color controls window
     def OpenControls(self):
 
         cv2.namedWindow("Color Controls")
         cv2.resizeWindow("Color Controls", 640, 250)
+
+        # The following 6 lines will create 6 trackbars that will allow the user to control
         cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, self.empty)
         cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, self.empty)
         cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, self.empty)
         cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, self.empty)
         cv2.createTrackbar("Val Min", "TrackBars", 0, 255, self.empty)
         cv2.createTrackbar("Val Max", "TrackBars", 255, 255, self.empty)
+
+    # This method will
+    def GetColorControlVals(self):
+        HueMin = cv2.getTrackbarPos("Hue Min", "TrackBars")
+        SatMin = cv2.getTrackbarPos("Sat Min", "TrackBars")
+        ValMin = cv2.getTrackbarPos("Val Min", "TrackBars")
+        HueMax = cv2.getTrackbarPos("Hue Max", "TrackBars")
+        SatMax = cv2.getTrackbarPos("Sat Max", "TrackBars")
+        ValMax = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+        HSVValues = {"HueMin": HueMin, "SatMin": SatMin, "ValMin": ValMin,
+                     "HueMax": HueMax, "SatMax": SatMax, "ValMax": ValMax}
+
+        # this line will print all the values above onto the terminal at runtime
+        print(HSVValues)
+
+        # This will return the the values
+        return HSVValues
