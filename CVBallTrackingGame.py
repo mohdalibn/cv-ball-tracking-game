c9b8ec7ee35537ba0c81d19aed4acd18038b8604
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 21:05:04 +0300

    Added the while loop with quiting functionality

--- CVBallTrackingGame.py
+++ CVBallTrackingGame.py
@@ -15,3 +15,17 @@ CamVideo = cv2.VideoCapture(0)
 # Setting the OpenCV Window Size
 CamVideo.set(3, 640)
 CamVideo.set(4, 480)
+
+
+run = True  # Variable to control the while loop below
+
+while run:
+
+    success, frame = CamVideo.read()
+
+    # Stops the window if the 'q' button on the keyboard is pressed
+    key = cv2.waitKey(1)
+    if key == 81 or key == 113:
+        run = False
+        cv2.destroyAllWindows()
+        break
