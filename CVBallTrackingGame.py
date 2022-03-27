bb3a16d9c49092c95b2bfafdbd2e5ead0e431571
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-03-27 20:55:20 +0300

    Created the main python file of the project and add the required libraries

---
+++ CVBallTrackingGame.py
@@ -0,0 +1,11 @@
+
+"""
+    PROJECT: CV Ball Tracking Game
+    MADE BY: Mohd Ali Bin Naser
+    Github : github.com/mohdalibn
+
+"""
+
+# Importing the required libraries for the project
+from HSVColorDetector import ColorDetector
+import cv2
