6d6b941b9242fa461e9d43967790a45a6d568fd6
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-04-02 00:15:00 +0300

    Replaced the Scale animation with the Alpha animation for the main menu script

--- Unity/CV Ball Tracking Game/Assets/MainMenu.cs
+++ Unity/CV Ball Tracking Game/Assets/MainMenu.cs
@@ -10,21 +10,39 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
-        // fader.gameObject.SetActive(true);
-        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
-        LeanTween.scale(fader, Vector3.zero, 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() =>{
+
+        // ALPHA/OPACITY ANIMATION
+        fader.gameObject.SetActive(true);
+        LeanTween.alpha(fader, 1, 0);
+        LeanTween.alpha(fader, 0, 0.5f).setOnComplete(() =>{
             fader.gameObject.SetActive(false);
         });
+
+        // SCALE ANIMATION
+        // fader.gameObject.SetActive(true);
+        // LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        // LeanTween.scale(fader, Vector3.zero, 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() =>{
+        //     fader.gameObject.SetActive(false);
+        // });
     }
 
     // This method will be called wheneve the play button is pressed. Use the the "using UnityEngine.SceneMangement;"
     public void PlayGame()
     {
         fader.gameObject.SetActive(true);
-        LeanTween.scale(fader, Vector3.zero, 0f);
-        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+
+        // ALPHA/OPACITY ANIMATION
+        LeanTween.alpha(fader, 0, 0);
+        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() =>{
             SceneManager.LoadScene("CV Ball Tracking Game");
         });
+
+
+        // SCALE ANIMATION
+        // LeanTween.scale(fader, Vector3.zero, 0f);
+        // LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() => {
+        //     SceneManager.LoadScene("CV Ball Tracking Game");
+        // });
         
     }
 
