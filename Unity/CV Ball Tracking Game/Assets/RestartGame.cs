a546cf174ab736ac9e00cbb342482c95027d6c4d
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-04-01 23:56:15 +0300

    Added fade out from game to main menu

--- Unity/CV Ball Tracking Game/Assets/RestartGame.cs
+++ Unity/CV Ball Tracking Game/Assets/RestartGame.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
+
+    [SerializeField] RectTransform fader;
+
+    private void Start()
+    {
+        // fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setOnComplete(() =>{
+            fader.gameObject.SetActive(false);
+        });
+    }
+
+
     // This method will be used to reload the game when the user clicks on the reload button
     public void ReloadGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+   
     // This method will be used to go back to the Main Menu Scene when the back button is pressed
     public void BackToMenu()
     {
-        SceneManager.LoadScene("Main Menu");
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, Vector3.zero, 0f);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setOnComplete(() => {
+            SceneManager.LoadScene("Main Menu");
+        });
+        
     }
 
 }
