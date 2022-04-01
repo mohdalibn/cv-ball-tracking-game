b92455d5a88ec718e38a95de73658bb1a0b69405
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-04-01 23:46:23 +0300

    Addd LeanTween Package in Unity and Fade out animation from Main Menu To Game Scene

--- Unity/CV Ball Tracking Game/Assets/MainMenu.cs
+++ Unity/CV Ball Tracking Game/Assets/MainMenu.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] RectTransform fader;
+
+    private void Start()
+    {
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.5f).setOnComplete(() =>{
+            fader.gameObject.SetActive(false);
+        });
+    }
+
     // This method will be called wheneve the play button is pressed. Use the the "using UnityEngine.SceneMangement;"
     public void PlayGame()
     {
-        SceneManager.LoadScene("CV Ball Tracking Game");
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, Vector3.zero, 0f);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.5f).setOnComplete(() => {
+            SceneManager.LoadScene("CV Ball Tracking Game");
+        });
+        
     }
 
     // This method will be called when the Quit button is pressed in the main menu
